00801780ccb63e4de4c7483d66ea805c682f6c11
lokaimoma <kelvinclark2k77@gmail.com>
2025-11-06 23:14:19 +0100

    Create creator tipjar

--- anchor_project/tip-jar/programs/tip-jar/src/instructions/initialize.rs
+++ anchor_project/tip-jar/programs/tip-jar/src/instructions/initialize.rs
@@ -1,9 +1,19 @@
 use anchor_lang::prelude::*;
 
+use crate::TipJar;
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'a> {
+    #[account(mut)]
+    pub creator: Signer<'a>,
+    #[account(init, space = 8 + TipJar::INIT_SPACE, payer = creator, seeds = [b"tipJar", creator.key().as_ref()], bump)]
+    pub tip_jar: Account<'a, TipJar>,
+    pub system_program: Program<'a, System>,
+}
 
 pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
+    let tip_jar = &mut ctx.accounts.tip_jar;
+    tip_jar.bump = ctx.bumps.tip_jar;
+    tip_jar.creator = ctx.accounts.creator.key();
     Ok(())
 }
--- anchor_project/tip-jar/programs/tip-jar/src/state.rs
+++ anchor_project/tip-jar/programs/tip-jar/src/state.rs
@@ -2,14 +2,14 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-struct TipJar {
+pub struct TipJar {
     pub creator: Pubkey,
     pub bump: u8,
 }
 
 #[account]
 #[derive(InitSpace)]
-struct TipRecord {
+pub struct TipRecord {
     pub tipper: Pubkey,
     pub tip_jar: Pubkey,
     pub bump: u8,
